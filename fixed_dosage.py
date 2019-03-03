2b86651d64ae20e43cf29c525cf2e249db3164c5
Billy Wan <billywxz@gmail.com>
2019-03-03 11:36:44 -0800

    fixed dosage baseline

---
+++ fixed_dosage.py
@@ -0,0 +1,18 @@
+'''
+Evaluate the performance of fixed-dosage baseline.
+'''
+
+import pandas as pd
+
+df = pd.read_csv("data/warfarin-discrete.csv")
+
+correct = 0
+total = 0
+for dose in df["Classified Dose of Warfarin"]:
+    total += 1
+    if dose == 1:
+        correct += 1
+
+print("Correct: ", correct)
+print("Total: ", total)
+print("Performance: ", 1.0 * correct / total)
