1931285d78465e7ce896a685746871d0d21b1834
Billy Wan <billywxz@gmail.com>
2019-03-03 11:27:49 -0800

    add classification script

--- classify-dosage.py
+++ classify-dosage.py
@@ -1,1 +1,15 @@
+import pandas as pd
 
+dataFrame = pd.read_csv("data/warfarin.csv")
+
+classified_dose = []
+for dose in dataFrame["Therapeutic Dose of Warfarin"]:
+    if dose < 21:
+        classified_dose.append(0)
+    elif dose > 49:
+        classified_dose.append(2)
+    else:
+        classified_dose.append(1)
+dataFrame["Classified Dose of Warfarin"] = classified_dose
+
+dataFrame.to_csv("data/warfarin-discrete.csv")
