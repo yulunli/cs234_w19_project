9bf3365e8a44af619f479bd06e36aec027955a9c
Billy Wan <billywxz@gmail.com>
2019-03-13 23:41:43 -0700

    add evaluation for linUCB

--- linUCB.py
+++ linUCB.py
@@ -8,11 +8,11 @@ K = 3
 
 df = pd.read_csv("data/warfarin_imputed.csv")
 labels = df["Classified Dose of Warfarin"]
-# using 1 feature for now
+# 3 features
 rs9923231 = df["VKORC1 -1639 consensus"]
 rs8050894 = df["VKORC1 1542 consensus"]
 cyp2c9 = df["CYP2C9 consensus"]
-# features: [5700, 4]
+# features: [5700, 15]
 d = 15
 features = []
 for row in rs9923231:
@@ -54,43 +54,61 @@ for i in range(len(cyp2c9)):
         features[i] += [0,0,0,0,0,0,1]
 features = np.array(features)
 
-As = [np.identity(d) for _ in range(K)]
-bs = [np.zeros(d) for _ in range(K)]
+data = np.array(list(zip(features, labels)))
+# split into train and test
+np.random.shuffle(data)
+train, test = data[:5000, :], data[5000:, :]
 alpha = 0.8 # hyperparameter
-total = 0
-correct = 0
-for f, y in zip(features, labels):
-    total += 1
+
+def main():
+    regrets = []
+    fractions = []
+    for _ in range(10): # train 10 times in different order to get avg and error bars
+        np.random.shuffle(train)
+        regret = []
+        fraction = []
+        As = [np.identity(d) for _ in range(K)]
+        bs = [np.zeros(d) for _ in range(K)]
+        fraction.append(evaluate(As, bs)) # performance without training
+        total = 0
+        correct = 0
+        for f, y in train:
+            total += 1
+            invAs = [np.linalg.inv(As[i]) for i in range(K)]
+            thetas = [np.matmul(invAs[i], bs[i]) for i in range(K)]
+            ps = [np.dot(thetas[i], f) + alpha * np.sqrt(np.dot(np.squeeze(np.matmul(f[None, :], invAs[i])), f)) for i in range(K)]
+            # a: chosen arm
+            a = np.argmax(ps)
+            if a == y:
+                r = 0
+                correct += 1
+            else:
+                r = -1
+            As[a] += np.matmul(f[:, None], f[None, :])
+            bs[a] += r * f
+            # evaluate every 500 examples
+            if total % 500 == 0:
+                regret.append(total - correct)
+                fraction.append(evaluate(As, bs))
+        print(fraction)
+        regrets.append(regret)
+        fractions.append(fraction)
+        print("Correct:", correct)
+        print("Total:", total)
+        print("Performance:", 1.0 * correct / total)
+
+# evaluation of fraction of incorrect dosing decisions with fixed weights
+def evaluate(As, bs):
     invAs = [np.linalg.inv(As[i]) for i in range(K)]
     thetas = [np.matmul(invAs[i], bs[i]) for i in range(K)]
-    ps = [np.dot(thetas[i], f) + alpha * np.sqrt(np.dot(np.squeeze(np.matmul(f[None, :], invAs[i])), f)) for i in range(K)]
-    # a: chosen arm
-    a = np.argmax(ps)
-    if a == y:
-        r = 0
-        correct += 1
-    else:
-        r = -1
-    As[a] += np.matmul(f[:, None], f[None, :])
-    bs[a] += r * f
+    incorrect = 0
+    for f, y in test:
+        ps = [np.dot(thetas[i], f) + alpha * np.sqrt(np.dot(np.squeeze(np.matmul(f[None, :], invAs[i])), f)) for i in range(K)]
+        # a: chosen arm
+        a = np.argmax(ps)
+        if a != y:
+            incorrect += 1
+    return 1.0 * incorrect / 700
 
-print("Correct:", correct)
-print("Total:", total)
-print("Performance:", 1.0 * correct / total)
-
-# evaluation
-total = 0
-correct = 0
-for f, y in zip(features, labels):
-    total += 1
-    invAs = [np.linalg.inv(As[i]) for i in range(K)]
-    thetas = [np.matmul(invAs[i], bs[i]) for i in range(K)]
-    ps = [np.dot(thetas[i], f) + alpha * np.sqrt(np.dot(np.squeeze(np.matmul(f[None, :], invAs[i])), f)) for i in range(K)]
-    # a: chosen arm
-    a = np.argmax(ps)
-    if a == y:
-        correct += 1
-
-print("Correct:", correct)
-print("Total:", total)
-print("Performance:", 1.0 * correct / total)
+if __name__ == '__main__':
+    main()
